b6f3b613710f88cb7ce9c0298e9086c1ee740f88
Halim Oumaima <oumaimalmao23@gmail.com>
2023-05-18 21:02:23 +0100

    Update.

---
+++ Day02/loops.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Setting up a basic input reading mechanism.
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
