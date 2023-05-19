d11dea7556c662504cd84b7885e5c00151e7accd
Halim Oumaima <oumaimalmao23@gmail.com>
2023-05-20 00:10:20 +0100

    Added try_catch.js

---
+++ Day03/try_catch.js
@@ -0,0 +1,39 @@
+
+// Setting up a basic input reading mechanism.
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function reverseString(s) {
+    try {
+        const str = s.split("").reverse().join("");
+        console.log(str);
+    } catch (error) {
+        console.log(error.message.split('\n')[0]);
+        console.log(s);
+    } 
+}
+
+function main() {
+    const s = eval(readLine());
+    
+    reverseString(s);
+}
