71035f917957af4f9c90ff17637ba576156d2b9b
Halim Oumaima <oumaimalmao23@gmail.com>
2023-05-20 03:01:22 +0100

    Added Day00.

---
+++ Day00/hello_world.js
@@ -0,0 +1,36 @@
+// Setting up a basic input reading mechanism.
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function greeting(parameterVariable) {
+    console.log('Hello, World!');
+}
+
+
+function main() {
+    const parameterVariable = readLine();
+    
+    greeting(parameterVariable);
+}
